1331bbe73e39d4a728db67b19f9541dff3aad8db
obusandy <obungasandrah@gmail.com>
2024-05-30 14:19:08 +0000

    Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' returns a list of lists of integers'''
+
+def pascal_triangle(n):
+    '''
+    Pascal's triangle
+    Argumentss:
+      n (interger): no of rows of the triangle
+    Returns:
+      List of lists of intgs rep the Pascal’s tri.
+    '''
+    trianglelist = []
+    if n == 0:
+        return lists
+    for integr in range(n):
+        trianglelist.append([])
+        trianglelist[integr].append(1)
+        if (integr > 0):
+            for r in range(1, integr):
+                trianglelist[integr].append(trianglelist[integr - 1][r - 1] + trianglelist[integr - 1][r])
+            trianglelist[integr].append(1)
+    return trianglelist
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+This is a read me file for the roject 0x00-pascal_triangle
