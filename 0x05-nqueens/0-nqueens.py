37127946f580cd4e0d2e6704e7aca4675d6c2087
obusandy <sandraobunga29@gmail.com>
2024-07-04 16:12:27 +0300

    my changes

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -8,7 +8,7 @@ N must be an int greater or equal to 4.
 import sys
 
 
-def n_queens():
+def nqueens():
     """
     Main function to handle input
     """
@@ -30,40 +30,40 @@ def n_queens():
     solvenqueens(chessbd, 0, sizen)
 
 
-def solvenqueens(chessbd, colmn, sizen):
+def solvenqueens(chessbd, col, sizen):
     """
     solves the N queens problem
     user called the program with the wrong number of args,
     print Usage: nqueens N
 
     """
-    if colmn == sizen:
+    if col == sizen:
         print_board(chessbd)
         return True
 
     respns = False
     for k in range(sizen):
-        if is_safe(chessbd, k, colmn, sizen):
-            chessbd[k][colmn] = 1
-            respns = solvenqueens(chessbd, colmn + 1, sizen) or respns
-            chessbd[k][colmn] = 0
+        if is_safe(chessbd, k, col, sizen):
+            chessbd[k][col] = 1
+            respns = solvenqueens(chessbd, col + 1, sizen) or respns
+            chessbd[k][col] = 0
     return respns
 
 
-def is_safe(chessbd, row, colmn, sizen):
+def is_safe(chessbd, row, col, sizen):
     """
     Checks if a queen can be placed at the given position on the chessboard
     without being attacked.
     """
-    for k in range(colmn):
+    for k in range(col):
         if chessbd[row][k] == 1:
             return False
 
-    for k, l in zip(range(row, -1, -1), range(colmn, -1, -1)):
+    for k, l in zip(range(row, -1, -1), range(col, -1, -1)):
         if chessbd[k][l] == 1:
             return False
 
-    for k, l in zip(range(row, sizen, 1), range(colmn, -1, -1)):
+    for k, l in zip(range(row, sizen, 1), range(col, -1, -1)):
         if chessbd[k][l] == 1:
             return False
 
@@ -85,4 +85,4 @@ def print_board(chessbd):
 
 
 if __name__ == "__main__":
-    n_queens()
+    nqueens()
